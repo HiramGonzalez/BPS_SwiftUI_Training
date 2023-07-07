e9fd9d5ae91c5f2531bcec83732bd46b40c79415
BPS.Dev01 <bps.dev01@BPSLLCs-Mac-mini.local>
2023-07-07 12:16:20 -0600

    Chapter 18 completed

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by BPS.Dev01 on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
